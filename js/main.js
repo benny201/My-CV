acb8028e179b0deb203759ae382d59d19d5ebd92
447093943@qq.com <zwu58@ucsc.edu>
2017-04-02 20:12:27 -0700

    index page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	$('#pagepiling').pagepiling({
+	    menu: null,
+        direction: 'vertical',
+        verticalCentered: true,
+        sectionsColor: ['#F0B27A', '#95A5A6', '#707B7C', '#797D7F'],
+        anchors: [],
+        scrollingSpeed: 700,
+        easing: 'swing',
+        loopBottom: false,
+        loopTop: false,
+        css3: true,
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['section1', 'section2', 'section3', 'section4']
+        },
+       	normalScrollElements: null,
+        normalScrollElementTouchThreshold: 5,
+        touchSensitivity: 5,
+        keyboardScrolling: true,
+        sectionSelector: '.section',
+        animateAnchor: false,
+
+		//events
+		onLeave: function(index, nextIndex, direction){},
+		afterLoad: function(anchorLink, index){},
+		afterRender: function(){},
+	});
+});
+
+// $(document).ready(function() {
+//     $('#pagepiling').pagepiling();
+// });
